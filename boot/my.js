be32dab84ef2f4d59fdbbfa27acbd77e4b143604
BraverChiang <huanfa2020@gmail.com>
2019-04-04 17:22:10 +0800

    按钮+链接

--- boot/my.js
+++ boot/my.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
 	
 	
-	$("#someid").css("color", "blue");
+//	$("#someid").css("color", "blue");
 	
 	
+	$("#直线与圆").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/解析几何1直线与圆.html"
+	});
 	
+	$("#圆锥曲线").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/解析几何2圆锥曲线.html"
+	});
+	
+	$("#解析大题").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/解析几何3解析大题.html"
+	});
 	
 });
