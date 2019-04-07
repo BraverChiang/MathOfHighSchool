4159baf52ad1425571d7cf7b7ec6e0f007bf6ffa
BraverChiang <huanfa2020@gmail.com>
2019-04-07 15:09:19 +0800

    前6章

--- my.js
+++ my.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 	
 	
 //	$("#someid").css("color", "blue");
+//第一章: 基础
 	$("#集合逻辑").click(function () {
 		window.location = "https://xiaojiang.tk/知识点/基础1集合逻辑.html"
 	});
@@ -18,8 +19,71 @@ $(document).ready(function () {
 		window.location = "https://xiaojiang.tk/知识点/基础4复数.html"
 	});
 	
+//第二章: 函数
+	$("#函数类型").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/函数1函数类型.html"
+	});
 	
+	$("#函数性质").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/函数2函数性质.html"
+	});
 	
+	$("#函数图像").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/函数3函数图像.html"
+	});
+	
+
+//第三章: 三角函数
+
+	$("#单位圆").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/三角函数1单位圆.html"
+	});
+	
+	$("#和差倍角").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/三角函数2和差倍角.html"
+	});
+	
+	$("#图像性质").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/三角函数3图像性质.html"
+	});
+	
+	$("#解三角").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/三角函数4解三角.html"
+	});
+	
+//第四章: 向量
+	$("#长度角度").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/向量.html"
+	});
+	
+	$("#向量运算").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/向量.html"
+	});
+	
+	$("#垂直平行").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/向量.html"
+	});
+	
+	$("#基底坐标").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/向量.html"
+	});
+
+
+//第五章: 立体几何
+	$("#三视图").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/立体几何1三视图.html"
+	});
+	
+	$("#线面关系").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/立体几何2线面关系.html"
+	});
+	
+	$("#空间角度").click(function () {
+		window.location = "https://xiaojiang.tk/知识点/立体几何3空间角.html"
+	});
+	
+
+//第六章: 解析几何
 	
 	
 	
@@ -34,6 +98,14 @@ $(document).ready(function () {
 	$("#解析大题").click(function () {
 		window.location = "https://xiaojiang.tk/知识点/解析几何3解析大题.html"
 	});
+	
+	
+//第七章: 概率统计
+
+//第八章: 数列
+
+//第九章: 导数
+
 	
 });
 
