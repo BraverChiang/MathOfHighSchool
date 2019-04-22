240fe46f49eeeebb6c450460cc81e9b9b8ba0263
BraverChiang <huanfa2020@gmail.com>
2019-04-23 00:49:38 +0800

    新界面, 纪念世界读书日

--- my.js
+++ my.js
@@ -1,174 +1,72 @@
+
+
+
 $(document).ready(function () {
-	
-	
-//	$("#someid").css("color", "blue");
-//第一章: 基础
-	$("#集合").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/开篇1集合.html"
-	});
-	
-	$("#逻辑").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/开篇2逻辑.html"
-	});
-	
-	$("#不等式").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/开篇3不等式.html"
-	});
-	
-	$("#线性规划").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/开篇4线性规划.html"
-	});
-	
-	$("#复数").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/开篇5复数.html"
-	});
-	
-	$("#初中").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/开篇6初中.html"
-	});
-	
-	
-//第二章: 函数
-	$("#指数对数").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/函数1指数对数.html"
-	});
-	
-	$("#初等函数").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/函数2初等函数及图像性质.html"
-	});
-	
-	$("#综合函数").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/函数3综合函数及图像性质.html"
-	});
-	
-	$("#分类讨论").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/函数4分类讨论.html"
-	});
-	
 
-//第三章: 三角函数
+	var  mainURL = "https://xiaojiang.tk/"
+	var  startChapter = ["开篇",       "集合","逻辑","不等式","线性规划","复数","初中"];
+	var  mathFunction = ["函数",       "指数对数", "初等函数及图像性质", "综合函数及图像性质", "分类讨论"];
+	var  trigonometric = ["三角函数",   "单位圆", "和差倍角", "图像性质", "解三角"]
+	var  vector = ["向量",             "平面向量", "空间向量"]
+	var  solidGeo = ["立体几何",        "几何体", "线面关系", "空间角", "空间向量法"]
+	var  analyticGeo = ["解析几何",     "直线与圆", "圆锥曲线", "解析大题"]
+	var  derivative = ["导数",         "切线斜率", "求导运算", "求极值", "构造原函数"]
+	var  sequence = ["数列",           "等差等比", "通项与前n项和", "数学归纳法"]
+	var  probability = ["概率统计",     "统计", "排列组合","二项式","随机事件与随机变量"]
+	var  allChapter = [startChapter, mathFunction, trigonometric, vector, solidGeo, 
+		analyticGeo, derivative, sequence, probability];
 
-	$("#单位圆").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/三角函数1单位圆.html"
-	});
-	
-	$("#和差倍角").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/三角函数2和差倍角.html"
-	});
-	
-	$("#图像性质").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/三角函数3图像性质.html"
-	});
-	
-	$("#解三角").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/三角函数4解三角.html"
-	});
-	
-//第四章: 向量
-	$("#平面向量").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/向量1平面向量.html"
-	});
-	
-	$("#空间向量").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/向量2空间向量.html"
-	});
-	
+	// 很不错, 就这样把, 可以少些很多代码
+	$(".section").click(function () {
+		console.log($(this).html());
 
-//第五章: 立体几何
-	$("#几何体").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/立体几何1几何体.html"
+		for (let j = 0; j < allChapter.length; j++) {
+			const element = allChapter[j];
+			for (var i = 1; i < element.length ; i++) {
+				if ($(this).html() == element[i]) {
+					console.log(i);
+					let string = mainURL+"知识点/"+element[0]+i+element[i]+".html"
+					console.log(string);
+					window.location = string
+				}
+			}
+		}
+		
 	});
-	
-	$("#线面关系").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/立体几何2线面关系.html"
-	});
-	
-	$("#空间角").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/立体几何3空间角.html"
-	});
-	
-	$("#空间向量法").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/立体几何4空间向量法.html"
-	});
-	
-	
-	
 
-//第六章: 解析几何
-	
-	
-	
-	$("#直线与圆").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/解析几何1直线与圆.html"
-	});
-	
-	$("#圆锥曲线").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/解析几何2圆锥曲线.html"
-	});
-	
-	
-	$("#解析大题").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/解析几何3解析大题.html"
-	});
-	
-	
-	
-	
-//第七章: 导数
+	$(".sectionExam").click(function () {
+		console.log($(this).html());
 
+		for (let j = 0; j < allChapter.length; j++) {
+			const element = allChapter[j];
+			for (var i = 1; i < element.length ; i++) {
+				if ($(this).html() == element[i]) {
+					console.log(i);
+					let string = mainURL+"高考真题/"+element[0]+i+element[i]+"/"+element[0]+i+element[i]+".html"
+					console.log(string);
+					window.location = string
+				}
+			}
+		}
+		
+	});
 
-	$("#切线斜率").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/导数1切线斜率.html"
-	});
-	
-	$("#求导运算").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/导数2求导运算.html"
-	});
-	
-	$("#求极值").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/导数3求极值.html"
-	});
-	
-	$("#构造原函数").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/导数4构造原函数.html"
-	});
-	
-//第八章: 数列
-	
-	$("#等差等比").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/数列1等差等比.html"
-	});
-	
-	$("#通项与前n项和").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/数列2通项与前n项和.html"
-	});
-	
-	$("#数学归纳法").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/数列3数学归纳法.html"
-	});
-	
-	
-	
-//第九章: 概率统计
-	
-	$("#统计").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/概率统计1统计.html"
-	});
-	
-	$("#排列组合").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/概率统计2排列组合.html"
-	});
-	
-	$("#二项式").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/概率统计3二项式.html"
-	});
-	
-	$("#随机事件与随机变量").click(function () {
-		window.location = "https://xiaojiang.tk/知识点/概率统计4随机事件与随机变量.html"
+	$(".sectionQA").click(function () {
+		console.log($(this).html());
+
+		for (let j = 0; j < allChapter.length; j++) {
+			const element = allChapter[j];
+			for (var i = 1; i < element.length ; i++) {
+				if ($(this).html() == element[i]) {
+					console.log(i);
+					let string = mainURL+"例题/"+element[0]+i+element[i]+"/"+element[0]+i+element[i]+".html"
+					console.log(string);
+					window.location = string
+				}
+			}
+		}
+		
 	});
 
 
-
-	
 });
-
