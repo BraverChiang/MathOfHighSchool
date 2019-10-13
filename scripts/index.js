36d5eae9b41b63f2ee901674185ed62f3ebb7d05
BraverChiang <huanfa2020@gmail.com>
2019-10-13 18:36:54 +0800

    buttom name 123 global function

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,41 @@
+	var  mainURL = "https://xiaojiang.tk/"
+	var  startChapter = ["开篇",       "集合","逻1辑","不等式","线性规划","复数","初中"];
+	var  mathFunction = ["函数",       "指数对数", "初2等函数", "综合函数", "分类讨论"];
+	var  trigonometric = ["三角函数",   "单位圆", "和3差倍角", "图像性质", "解三角"]
+	var  vector = ["向量",             "平面向量", "空4间向量"]
+	var  solidGeo = ["立体几何",        "几何体", "线面关系", "空间角", "空间向量法"]
+	var  analyticGeo = ["解析几何",     "直线与圆", "圆锥曲线", "解析大题"]
+	var  derivative = ["导数",         "切线斜率", "求导运算", "求极值", "构造原函数"]
+	var  sequence = ["数列",           "等差等比", "通项与前n项和", "数学归纳法"]
+	var  probability = ["概率统计",     "统计", "排列组合","二项式","随机事件与随机变量"]
+	var  allChapter = [startChapter, mathFunction, trigonometric, vector, solidGeo, 
+		analyticGeo, derivative, sequence, probability];
+	
+	function msgup_bar(){
+		//loading name from global var to button
+	
+		
+		for (var i = 0; i < allChapter.length ; i++) {
+//			$("#C1_L1").html('Save909');
+			const reali = i+1;  //2
+			const chapter = "C"+reali; //C2
+			const chapterName = allChapter[i]; //
+			const element = allChapter[i]
+			
+			for (var l=1; l<element.length; l++){
+				
+				const lesson = chapter+"_L"+l;
+				const lessonName = element[l]
+				
+				$('#'+lesson).html(lessonName);
+			}
+			
+		}
+//	
+//		
+//		$("#C1_L1").html('Save998');
+	};
 
-function msgup_bar(){
-$("#C1_L1").html('Save99');
-};
 
 
 $(document).ready(function () {
@@ -12,23 +46,25 @@ $(document).ready(function () {
 
 	
 	
+//
+//	var  mainURL = "https://xiaojiang.tk/"
+//	var  startChapter = ["开篇",       "集合","逻辑","不等式","线性规划","复数","初中"];
+//	var  mathFunction = ["函数",       "指数对数", "初等函数", "综合函数", "分类讨论"];
+//	var  trigonometric = ["三角函数",   "单位圆", "和差倍角", "图像性质", "解三角"]
+//	var  vector = ["向量",             "平面向量", "空间向量"]
+//	var  solidGeo = ["立体几何",        "几何体", "线面关系", "空间角", "空间向量法"]
+//	var  analyticGeo = ["解析几何",     "直线与圆", "圆锥曲线", "解析大题"]
+//	var  derivative = ["导数",         "切线斜率", "求导运算", "求极值", "构造原函数"]
+//	var  sequence = ["数列",           "等差等比", "通项与前n项和", "数学归纳法"]
+//	var  probability = ["概率统计",     "统计", "排列组合","二项式","随机事件与随机变量"]
+//	var  allChapter = [startChapter, mathFunction, trigonometric, vector, solidGeo, 
+//		analyticGeo, derivative, sequence, probability];
+		
+		
+		
 
-	var  mainURL = "https://xiaojiang.tk/"
-	var  startChapter = ["开篇",       "集合","逻辑","不等式","线性规划","复数","初中"];
-	var  mathFunction = ["函数",       "指数对数", "初等函数", "综合函数", "分类讨论"];
-	var  trigonometric = ["三角函数",   "单位圆", "和差倍角", "图像性质", "解三角"]
-	var  vector = ["向量",             "平面向量", "空间向量"]
-	var  solidGeo = ["立体几何",        "几何体", "线面关系", "空间角", "空间向量法"]
-	var  analyticGeo = ["解析几何",     "直线与圆", "圆锥曲线", "解析大题"]
-	var  derivative = ["导数",         "切线斜率", "求导运算", "求极值", "构造原函数"]
-	var  sequence = ["数列",           "等差等比", "通项与前n项和", "数学归纳法"]
-	var  probability = ["概率统计",     "统计", "排列组合","二项式","随机事件与随机变量"]
-	var  allChapter = [startChapter, mathFunction, trigonometric, vector, solidGeo, 
-		analyticGeo, derivative, sequence, probability];
-		
-		
-		
-		
+
+
 
 	// 很不错, 就这样把, 可以少些很多代码
 	$(".section").click(function () {
